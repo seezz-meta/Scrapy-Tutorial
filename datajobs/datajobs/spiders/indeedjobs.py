6f6eda97d8cfe97151261c370a94e98cd42d76f8
Sizwe Zwane <si.zwane147@gmail.com>
2023-07-13 21:34:56 +0200

    Added more tages to scrape jobLocationCity, jobLocationState and formatted_location

--- datajobs/datajobs/spiders/indeedjobs.py
+++ datajobs/datajobs/spiders/indeedjobs.py
@@ -8,7 +8,7 @@ from random import randint
 class IndeedjobsSpider(scrapy.Spider):
     name = "indeedjobs"
     allowed_domains = ["za.indeed.com"]
-    start_urls = ["https://za.indeed.com/jobs?q=Data+Analytics&l=South+Africa"]
+    start_urls = ["https://za.indeed.com/jobs?q=Data+Analytics&l=South+Africa&sort=date"]
 
     def parse(self, response):
         print(response.url)
@@ -21,18 +21,16 @@ class IndeedjobsSpider(scrapy.Spider):
                 if job.get('adBlob') is None:
                     job_link = 'https://za.indeed.com' + job['link']
                     extracted_salary = job.get('extractedSalary', np.nan)
-
+                    self.random_delay()  # Add random delay between requests
                     yield scrapy.Request(url=job_link,
                                         callback=self.parse_job,
-                                        meta=self.get_request_meta(job, extracted_salary))
-                    self.random_delay()  # Add random delay between requests
+                                        meta=self.get_request_meta(job, extracted_salary, page_num))
         
         # Follow the next page button if available
         next_button = response.css('a[data-testid="pagination-page-next"]::attr(href)').get()
         if next_button:
-            yield response.follow(next_button, callback=self.parse)
-        else:
-            print('Next Button Not Found')
+            self.random_delay()  # Add random delay between requests
+            yield scrapy.Request('https://za.indeed.com' + next_button, callback=self.parse)
 
     def parse_job(self, response):
         # Extract job details from the job listing page
@@ -43,7 +41,9 @@ class IndeedjobsSpider(scrapy.Spider):
             'company_review_count': response.meta['company_review_count'],
             'display_title': response.meta['display_title'],
             'relative_time': response.meta['relative_time'],
-            'location_city': response.meta['location_city'],
+            'job_location_city' : response.meta['jobLocationCity'],
+            'job_location_state' : response.meta['jobLocationState'],
+            'formatted_location': response.meta['formatted_location'],
             'extracted_salary': response.meta['extracted_salary'],
             'job_types': response.meta['job_types'],
             'job_link': response.meta['job_link'],
@@ -59,7 +59,9 @@ class IndeedjobsSpider(scrapy.Spider):
             'company_review_count': job['companyReviewCount'],
             'display_title': job['displayTitle'],
             'relative_time': job['formattedRelativeTime'],
-            'location_city': job['formattedLocation'],
+            'jobLocationCity': job['jobLocationCity'],
+            'jobLocationState' : job['jobLocationState'],
+            'formatted_location': job['formattedLocation'],
             'extracted_salary': extracted_salary,
             'job_types': job['jobTypes'],
             'job_link': 'https://za.indeed.com' + job['link'],
@@ -67,5 +69,5 @@ class IndeedjobsSpider(scrapy.Spider):
         }
 
     def random_delay(self):
-        delay = randint(1, 5)
+        delay = randint(0, 6)
         time.sleep(delay)
