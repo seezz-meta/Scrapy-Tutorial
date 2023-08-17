2e4610529ee30cac0746b9d8b41e00d251010be5
Sizwe Zwane <22715842+seezz-meta@users.noreply.github.com>
2023-08-17 10:06:59 +0200

    Update indeedjobs.py
    Cleaned the code

--- datajobs/datajobs/spiders/indeedjobs.py
+++ datajobs/datajobs/spiders/indeedjobs.py
@@ -11,7 +11,7 @@ class IndeedjobsSpider(scrapy.Spider):
     start_urls = ["https://za.indeed.com/jobs?q=Data+Analytics&l=South+Africa&sort=date"]
 
     def parse(self, response):
-        print(response.url)
+        page_num = response.url
         # Extract job details from the search page
         script_tag = re.findall(r'window.mosaic.providerData\["mosaic-provider-jobcards"\]=(\{.+?\});', response.text)
         if script_tag is not None:
@@ -19,22 +19,22 @@ class IndeedjobsSpider(scrapy.Spider):
             jobs_list = json_blob['metaData']['mosaicProviderJobCardsModel']['results']
             for job in jobs_list:
                 if job.get('adBlob') is None:
-                    job_link = 'https://za.indeed.com' + job['link']
-                    extracted_salary = job.get('extractedSalary', np.nan)
+                    job_link = 'https://za.indeed.com' + job.get('link', np.nan)
                     self.random_delay()  # Add random delay between requests
                     yield scrapy.Request(url=job_link,
                                         callback=self.parse_job,
-                                        meta=self.get_request_meta(job, extracted_salary, page_num))
+                                        meta=self.get_request_meta(job, page_num))
         
         # Follow the next page button if available
         next_button = response.css('a[data-testid="pagination-page-next"]::attr(href)').get()
         if next_button:
             self.random_delay()  # Add random delay between requests
-            yield scrapy.Request('https://za.indeed.com' + next_button, callback=self.parse)
+            yield response.follow('https://za.indeed.com' + next_button, callback=self.parse)
 
     def parse_job(self, response):
         # Extract job details from the job listing page
         yield {
+            'page_url': response.meta['page_url'],
             'job_id': response.meta['job_id'],
             'company': response.meta['company'],
             'company_rating': response.meta['company_rating'],
@@ -47,25 +47,28 @@ class IndeedjobsSpider(scrapy.Spider):
             'extracted_salary': response.meta['extracted_salary'],
             'job_types': response.meta['job_types'],
             'job_link': response.meta['job_link'],
+            'salary_from_job_link' : response.css('div.css-gle1f1.e1xnxm2i0 div.css-tvvxwd.ecydgvn1::text').get(default=np.nan),
+            'job_type_from_job_link' : response.css('div.css-m539th.eu4oa1w0 div.css-tvvxwd.ecydgvn1::text').get(default=np.nan),
             'pub_date': response.meta['pub_date'],
-            'job_description': response.css('div#jobDescriptionText').getall()
+            'job_description': response.css('div#jobDescriptionText').getall(),
         }
 
-    def get_request_meta(self, job, extracted_salary):
+    def get_request_meta(self, job, page_num):
         return {
-            'job_id': job['jobkey'],
-            'company': job['company'],
-            'company_rating': job['companyRating'],
-            'company_review_count': job['companyReviewCount'],
-            'display_title': job['displayTitle'],
-            'relative_time': job['formattedRelativeTime'],
-            'jobLocationCity': job['jobLocationCity'],
-            'jobLocationState' : job['jobLocationState'],
-            'formatted_location': job['formattedLocation'],
-            'extracted_salary': extracted_salary,
-            'job_types': job['jobTypes'],
-            'job_link': 'https://za.indeed.com' + job['link'],
-            'pub_date': job['pubDate'],
+            'job_id': job.get('jobkey', np.nan),
+            'company': job.get('company', np.nan),
+            'company_rating': job.get('companyRating', np.nan),
+            'company_review_count': job.get('companyReviewCount', np.nan),
+            'display_title': job.get('displayTitle', np.nan),
+            'relative_time': job.get('formattedRelativeTime', np.nan),
+            'jobLocationCity': job.get('jobLocationCity', np.nan),
+            'jobLocationState' : job.get('jobLocationState', np.nan),
+            'formatted_location': job.get('formattedLocation', np.nan),
+            'extracted_salary': job.get('extractedSalary', np.nan),
+            'job_types': job.get('jobTypes', np.nan),
+            'job_link': 'https://za.indeed.com' + job.get('link', np.nan),
+            'pub_date': job.get('pubDate', np.nan),
+            'page_url': page_num
         }
 
     def random_delay(self):
